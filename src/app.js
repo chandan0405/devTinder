796a1e781d4778b7a8a6ca76a6dd65e6b6406956
Chandan.Kumar <chandan04051999@gmail.com>
2026-04-05 17:09:16 +0530

    nested routing

--- src/app.js
+++ src/app.js
@@ -2,23 +2,39 @@ const express = require("express");
 
 const app = express();
 
+// url params
+// app.get('/user', (req, res) => {
+//     const id = req.query;
+//     console.log(id);
+//     res.send(`User id is logged`);
+// })
 
-app.get("/", (req, res) => {
-    res.send("Hello World from the server");
-})
-app.get('/about', (req, res) => {
-    res.send("About page ");
-})
-app.post("/about", (req, res) => {
-    res.send("About page updated successfully");
-})
-app.post("/contact", (req, res) => {
-    res.send("Contact page updated successfully");
-})
-// patch request
-app.patch('/about', (req, res) => {
-    res.send("about page is patched")
-})
+// app.post('/post', (req, res) => {
+//     const queryparams = req.query;
+//     console.log(queryparams);
+//     res.send("Post request is logged");
+// })
+
+// app.get('/user/:userId/:name/:age', (req, res) => {
+//     const { userId, name, age } = req.params;
+//     console.log(userId, name, age);
+//     res.send(`User id is ${userId}, name is ${name}, age is ${age}`);
+// })
+
+
+app.use("/user", (req, res, next) => {
+    console.log("first response from the user route");
+    next();
+    res.send("User route is logged");
+
+},
+    (req, res) => {
+        console.log("second response from the user route");
+        res.send("User route is logged second time");
+    }
+)
+
+
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
