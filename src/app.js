27fd4672a3f84769d92e8a2f4ba878eaaf26ab98
Chandan.Kumar <chandan04051999@gmail.com>
2026-04-05 23:45:35 +0530

    created dummy middleware with dummy admin verification

--- src/app.js
+++ src/app.js
@@ -1,38 +1,14 @@
 const express = require("express");
-
+const { adminAuth } = require("./middlewares/auth")
 const app = express();
 
-// url params
-// app.get('/user', (req, res) => {
-//     const id = req.query;
-//     console.log(id);
-//     res.send(`User id is logged`);
-// })
+// middleware for the admin authentication
 
-// app.post('/post', (req, res) => {
-//     const queryparams = req.query;
-//     console.log(queryparams);
-//     res.send("Post request is logged");
-// })
+app.use("/admin", adminAuth)
 
-// app.get('/user/:userId/:name/:age', (req, res) => {
-//     const { userId, name, age } = req.params;
-//     console.log(userId, name, age);
-//     res.send(`User id is ${userId}, name is ${name}, age is ${age}`);
-// })
-
-
-app.use("/user", (req, res, next) => {
-    console.log("first response from the user route");
-    next();
-    res.send("User route is logged");
-
-},
-    (req, res) => {
-        console.log("second response from the user route");
-        res.send("User route is logged second time");
-    }
-)
+app.use('/admin/addUser', (req, res) => {
+    res.send("user created successfully by admin")
+})
 
 
 app.listen(3000, () => {
---
+++ src/middlewares/auth.js
@@ -0,0 +1,14 @@
+const adminAuth = (req, res, next) => {
+    const token = "xyz";
+    const recievedToken = "xyz";
+    if (token === recievedToken) {
+        next();
+    }
+    else {
+        res.status(401).send("UnAuthorized player");
+    }
+}
+
+module.exports = {
+    adminAuth
+}
